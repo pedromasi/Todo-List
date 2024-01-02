78200fa974d04aa915c9ec0648488f619f07a1d2
Pedro Machado da Silva <54449463+pedromasi@users.noreply.github.com>
2024-01-02 11:58:04 -0300

    Todo List
    project finished

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,10 @@
+type Props = {
+    label: string;
+    onClick: VoidFunction;
+}
+
+export const CustomButton = ({ label, onClick }: Props) => {
+    return (
+        <button onClick={onClick} className="p-3 text-white bg-blue-700 rounded-md">{label}</button>
+    );
+}
---
+++ src/data/peopleList.ts
@@ -0,0 +1,9 @@
+import { Person } from "@/types/Person";
+
+export const peopleList: Person[] = [
+    { id: 1, name: 'Fulano', profession: 'mathematician' },
+    { id: 2, name: 'Ciclano', profession: 'baller' },
+    { id: 3, name: 'Beltrano', profession: 'filosopher' },
+    { id: 4, name: 'Glutano', profession: 'chemist' },
+    { id: 5, name: 'Jungulano', profession: 'cop' }
+];
